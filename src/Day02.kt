e47d6f531090489071eed5b20526cd778b58d06d
cunhap <pedro.cunha@blip.pt>
2023-12-02 10:58:56 +0000

    Refactor code for readability and modularity
    This commit extracts common code to get cubes from a round and to map the color to the amount into two new functions, resulting in clearer and more modularized code. It promotes code reuse and reduces redundant code blocks which enhances maintainability.

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,14 +3,22 @@ fun main() {
         return line.substringAfter(": ").split(";").map { it.trim() }
     }
 
+    fun cubesInRound(round: String): List<String> {
+        return round.split(",").map { it.trim() }
+    }
+
+    fun colorToAmount(cubes: List<String>): Map<String, Int> {
+        return cubes.map { it.split(" ") }.associate { it[1] to it[0].toInt() }
+    }
+
     fun part1(input: List<String>): Int {
         val possibleCubes = mapOf("red" to 12, "green" to 13, "blue" to 14)
         return input.sumOf { line ->
             val idOfGame = line.substringBefore(":").substringAfter("Game ").toInt()
             val showOfCubes = showOfCubes(line)
             val gamePossible = showOfCubes.all { round ->
-                val cubes = round.split(",").map { it.trim() }
-                val colorToAmountMap = cubes.map { it.split(" ") }.associate { it[1] to it[0].toInt() }
+                val cubes = cubesInRound(round)
+                val colorToAmountMap = colorToAmount(cubes)
                 colorToAmountMap.all { (color, amount) ->
                     possibleCubes[color]!! >= amount
                 }
@@ -19,15 +27,13 @@ fun main() {
         }
     }
 
-
-
     fun part2(input: List<String>): Int {
         return input.sumOf { line ->
-            val showOfCubes = showOfCubes(line)
             val minimalAmountOfCubes = mutableMapOf<String, Int>()
+            val showOfCubes = showOfCubes(line)
             showOfCubes.map { round ->
-                val cubes = round.split(",").map { it.trim() }
-                val colorToAmountMap = cubes.map { it.split(" ") }.associate { it[1] to it[0].toInt() }
+                val cubes = cubesInRound(round)
+                val colorToAmountMap = colorToAmount(cubes)
                 colorToAmountMap.forEach { (color, amount) ->
                     minimalAmountOfCubes.putIfAbsent(color, amount)
                     if(amount > minimalAmountOfCubes[color]!!) {
